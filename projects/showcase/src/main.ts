bbbd8ac35b1eafe091ec7dfbf3547722e3f790ef
Joseph Ng <joseph_sieang@live.com>
2023-01-06 15:09:29 +0800

    Import CommonModule and BrowserAnimationsModule

--- projects/showcase/src/main.ts
+++ projects/showcase/src/main.ts
@@ -1,4 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(CommonModule, BrowserAnimationsModule)],
+}).catch((err) => console.error(err));
