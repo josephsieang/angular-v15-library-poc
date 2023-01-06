2b40ab0d3c30c43c7f71a83842b4a1dcf8b447a5
Joseph Ng <joseph_sieang@live.com>
2023-01-06 15:09:03 +0800

    Implement lib loader

--- projects/josephsieang-ngx-spinner/src/lib/josephsieang-ngx-spinner.component.ts
+++ projects/josephsieang-ngx-spinner/src/lib/josephsieang-ngx-spinner.component.ts
@@ -1,15 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { NgxSpinnerService, Size } from 'ngx-spinner';
 
 @Component({
-  selector: 'lib-josephsieang-ngx-spinner',
+  selector: 'josephsieang-ngx-spinner',
   template: `
-    <p>
-      josephsieang-ngx-spinner works!
-    </p>
+    <!-- For fullScreen: false, please add position: relative to its parent -->
+    <ngx-spinner
+      [bdColor]="bdColor"
+      [size]="size"
+      [color]="color"
+      [type]="type"
+      [fullScreen]="fullScreen"
+      [name]="name"
+    >
+    </ngx-spinner>
   `,
-  styles: [
-  ]
+  styles: [],
 })
 export class JosephsieangNgxSpinnerComponent {
+  @Input() bdColor: string = 'rgba(0,0,0,0.4)';
+  @Input() size: Size = 'large';
+  @Input() color: string = '#fff';
+  @Input() type: string = 'ball-scale';
+  @Input() fullScreen: boolean = false;
+  @Input() isLoading: boolean = false;
+  @Input() name: string = 'loader';
 
+  constructor(private spinner: NgxSpinnerService) {}
+
+  ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const { isLoading } = changes;
+
+    if (isLoading && isLoading.currentValue) {
+      this.show();
+    } else {
+      this.hide();
+    }
+  }
+
+  private show(): void {
+    this.spinner.show(this.name);
+  }
+
+  private hide(): void {
+    this.spinner.hide(this.name);
+  }
 }
--- projects/josephsieang-ngx-spinner/src/lib/josephsieang-ngx-spinner.module.ts
+++ projects/josephsieang-ngx-spinner/src/lib/josephsieang-ngx-spinner.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { JosephsieangNgxSpinnerComponent } from './josephsieang-ngx-spinner.component';
 
-
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
-  declarations: [
-    JosephsieangNgxSpinnerComponent
-  ],
-  imports: [
-  ],
-  exports: [
-    JosephsieangNgxSpinnerComponent
-  ]
+  declarations: [JosephsieangNgxSpinnerComponent],
+  imports: [NgxSpinnerModule],
+  exports: [JosephsieangNgxSpinnerComponent],
 })
-export class JosephsieangNgxSpinnerModule { }
+export class JosephsieangNgxSpinnerModule {}
