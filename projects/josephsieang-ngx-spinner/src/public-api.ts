1cd1a725048ee4d6350cfa9931eac9753f060c01
Joseph Ng <joseph_sieang@live.com>
2023-01-06 15:11:30 +0800

    Remove unused lib service

--- projects/josephsieang-ngx-spinner/src/public-api.ts
+++ projects/josephsieang-ngx-spinner/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of josephsieang-ngx-spinner
  */
 
-export * from './lib/josephsieang-ngx-spinner.service';
 export * from './lib/josephsieang-ngx-spinner.component';
 export * from './lib/josephsieang-ngx-spinner.module';
