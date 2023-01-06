be1d69f93eb5f85eb301858975c9efe5aba69e3c
Joseph Ng <joseph_sieang@live.com>
2023-01-06 17:11:59 +0800

    Demo loader

--- projects/showcase/src/app/app.component.ts
+++ projects/showcase/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { JosephsieangNgxSpinnerModule } from 'josephsieang-ngx-spinner';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, JosephsieangNgxSpinnerModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
